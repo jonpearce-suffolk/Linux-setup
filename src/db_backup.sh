560226d2e4f412f71debd7cc092bacf3485f8b77
Jonathan Pearce <jon-git@multi-project.co.uk>
2023-08-14 14:37:38 +0100

    Update db_backup.sh
    Set as config options

--- src/db_backup.sh
+++ src/db_backup.sh
@@ -1,5 +1,7 @@
 # 0 18 * * * bash /path to script/db_backup.sh
 backup_path="/home/user/backup"; export backup_path;
+backup_passwd="fred"; export backup_passwd;
 candidates=$(echo "show databases" | mysql -ubackup -p8*backup |grep -Ev "^(Database|mysql|performance_schema|information_schema)$")
 echo $candidates
-mysqldump --databases $candidates --single-transaction --quick --lock-tables=false > $backup_path/DB-full-backup-$(date +%F).sql -u backup -p8*backup
+mysqldump --databases $candidates --single-transaction --quick --lock-tables=false > $backup_path/DB-full-backup-$(date +%F).sql -u backup -p$backup_passwd
+
