c6a0acbb0c294227de03798854e22a4f20e76a88
Jonathan Pearce <jon-git@multi-project.co.uk>
2023-08-14 13:34:01 +0100

    Update db_backup.sh
    Include cron line.

--- src/db_backup.sh
+++ src/db_backup.sh
@@ -1,3 +1,5 @@
+# 0 18 * * * bash /path to script/db_backup.sh
+backup_path="/home/user/backup"; export backup_path;
 candidates=$(echo "show databases" | mysql -ubackup -p8*backup |grep -Ev "^(Database|mysql|performance_schema|information_schema)$")
 echo $candidates
-mysqldump --databases $candidates --single-transaction --quick --lock-tables=false > /home/jonp/backup/DB-full-backup-$(date +%F).sql -u backup -p8*backup
+mysqldump --databases $candidates --single-transaction --quick --lock-tables=false > $backup_path/DB-full-backup-$(date +%F).sql -u backup -p8*backup
